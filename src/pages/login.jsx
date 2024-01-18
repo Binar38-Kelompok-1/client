3f4d5e3761472942cd325f7d94e912dd11cf4ce7
juliochandra <juliochandra@gmail.com>
2024-01-19 02:48:04 +0700

    update function login with save token to cookies and make function to send token to request

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,50 @@
-const Login = () => {
+import React, { useState } from "react";
+import axios from "axios";
 
-    
-    return (
-        <>
-            <style>
-                {`
+const Login = () => {
+  const [formData, setFormData] = useState({
+    nik: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3000/", formData);
+
+      // Assuming your API response structure is similar to the provided example
+      if (response.data.token) {
+        console.log("Login successful");
+        console.log("authorization:", response.data.token);
+        document.cookie = `authorization=${response.data.token}; path=/;`;
+
+        // cookie.set("authorization", response.data.token, {
+        //   httpOnly: true,
+        //   secure: false,
+        // });
+
+        // Handle successful login, such as storing the token in state or localStorage
+      } else {
+        console.log("Login failed");
+        // Handle login failure, show error message, etc.
+      }
+    } catch (error) {
+      console.error("An error occurred during login:", error.message);
+      // Handle error, show error message, etc.
+    }
+  };
+  return (
+    <>
+      <style>
+        {`
                     body {
                         font-family: 'Poppins' !important;
                     }
@@ -67,40 +107,61 @@ const Login = () => {
                         margin-top: -15px;
                     }
                 `}
-            </style>
+      </style>
 
-            <center>
-                <h1 className="mt-3 title">Aplikasi Laporan Masyarakat Depok</h1> <br/>
-                <h1 className="login">Login</h1>
-
-                {/* <% if (typeof message !== 'undefined' && message.length > 0) { %>
+      <center>
+        <h1 className="mt-3 title">Aplikasi Laporan Masyarakat Depok</h1> <br />
+        <h1 className="login">Login</h1>
+        {/* <% if (typeof message !== 'undefined' && message.length > 0) { %>
                     <p className="alert alert-success w-25" style="color: green;"><%= message %></p>
                     <a className="msg-btn-succ" href="/"><i className="fa-sharp fa-solid fa-xmark"></i></a>
                 <% } %> */}
-
-                {/* <% if (typeof errors !== 'undefined') { %>
+        {/* <% if (typeof errors !== 'undefined') { %>
                     <p className="alert alert-danger w-25" style="color: red;"><i className="fa-solid fa-circle-exclamation"></i> <%= errors[0].message %></p>
                     <a className="msg-btn-err" href="/"><i className="fa-sharp fa-solid fa-xmark"></i></a>
                 <% } %> */}
-                
-                <div className="container w-25 div-form">
-                    <form action="/" method="post">
-                        <div>
-                            <input className="form-control mb-2" type="number" id="nik" name="nik" placeholder="NIK" required/>
-                        </div>
-                        <div>
-                            <input className="form-control" type="password" id="password" name="password" placeholder="Password"/> <br/>
-                        </div>
-                        <button type="submit" className="btn btn-success mb-2 w-100 masuk">Masuk</button>
-                    </form>
-                </div> <br/>
+        <div className="container w-25 div-form">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <input
+                className="form-control mb-2"
+                type="number"
+                id="nik"
+                name="nik"
+                placeholder="NIK"
+                value={formData.nik}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div>
+              <input
+                className="form-control"
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />{" "}
+              <br />
+            </div>
+            <button type="submit" className="btn btn-success mb-2 w-100 masuk">
+              Masuk
+            </button>
+          </form>
+        </div>{" "}
+        <br />
+        <p>
+          Belum punya akun? Register <a href="/register">disini</a>
+        </p>
+        <p>
+          Petugas login <a href="/login-petugas">disini</a>
+        </p>
+        <img src="logo.png" alt="" />
+      </center>
+    </>
+  );
+};
 
-                <p>Belum punya akun? Register <a href="/register">disini</a></p>
-                <p>Petugas login <a href="/login-petugas">disini</a></p>
-                <img src="logo.png" alt=""/>
-            </center>
-        </>
-    )
-}
-
-export default Login
+export default Login;
--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -1,8 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 const UserProfile = () => {
-    return (
-        <>
-            <style>
-                {`
+  const [userData, setUserData] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = document.cookie
+          .split("; ")
+          .find((row) => row.startsWith("authorization="))
+          .split("=")[1];
+
+        const response = await axios.get("http://localhost:3000/user", {
+          withCredentials: true,
+          headers: {
+            Authorization: ` ${token}`,
+          },
+        });
+
+        if (response.data.message === "success") {
+          setUserData(response.data.data);
+        } else {
+          setError("Failed to fetch user data");
+        }
+      } catch (error) {
+        setError("An error occurred while fetching user data");
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  return (
+    <>
+      <style>
+        {`
                     .card-header {
                         background-color: #2E3691;
                         color: white;
@@ -45,42 +79,56 @@ const UserProfile = () => {
                         margin-bottom: -30px;
                     }
                 `}
-            </style>
+      </style>
 
-            <div className="container w-50 text-center">
-                {/* <% if (message.length > 0) { %>
-                    <p className="alert alert-success mt-5" style="color: green;"><%= message %></p>
-                <% } %> */}
-            </div>
+      <div className="container w-50 text-center">
+        {error && (
+          <p className="alert alert-danger mt-5" style={{ color: "red" }}>
+            {error}
+          </p>
+        )}
+      </div>
 
-            <div className="container w-50 prof-cont">
-                <div className="card">
-                    <div className="card-header text-center">
-                        Profil Anda
-                    </div>
-                    <div className="card-body">
-                        <p className="key"><span>NIK:</span>
-                            {/* <%= data.nik %> */}
-                        </p>
-                        <p className="key"><span>Nama Lengkap:</span>
-                            {/* <%= data.nama %> */}
-                        </p>
-                        <p className="key"><span>Nomor Telepon:</span>
-                            {/* <%= 0+data.no_telp %> */}
-                        </p>
-                        <p className="key"><span>Alamat:</span>
-                            {/* <%= data.alamat %> */}
-                        </p>
-                    </div>
-                    <div className="card-footer d-flex justify-content-between">
-                        <a className="btn btn-danger" style={{width: '19%'}} href="/user"><i className="fa-solid fa-right-from-bracket"></i></a>
-                        <a className="btn btn-success" style={{width: '39%'}} href="profil/edit"><i className="fa-solid fa-pen-to-square"></i> Edit Profil</a>
-                        <a className="btn btn-primary" style={{width: '39%'}} href="profil/password"><i className="fa-solid fa-lock"></i> Ubah Password</a>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+      <div className="container w-50 prof-cont">
+        <div className="card">
+          <div className="card-header text-center">Profil Anda</div>
+          <div className="card-body">
+            <p className="key">
+              <span>NIK:</span> {userData.nik}
+            </p>
+            <p className="key">
+              <span>Nama Lengkap:</span> {userData.nama}
+            </p>
+            <p className="key">
+              <span>Nomor Telepon:</span> {userData.no_telp}
+            </p>
+            <p className="key">
+              <span>Alamat:</span> {userData.alamat}
+            </p>
+          </div>
+          <div className="card-footer d-flex justify-content-between">
+            <a className="btn btn-danger" style={{ width: "19%" }} href="/user">
+              <i className="fa-solid fa-right-from-bracket"></i>
+            </a>
+            <a
+              className="btn btn-success"
+              style={{ width: "39%" }}
+              href="profil/edit"
+            >
+              <i className="fa-solid fa-pen-to-square"></i> Edit Profil
+            </a>
+            <a
+              className="btn btn-primary"
+              style={{ width: "39%" }}
+              href="profil/password"
+            >
+              <i className="fa-solid fa-lock"></i> Ubah Password
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default UserProfile
+export default UserProfile;
